7c0990e574c3181430f489f4b8a3d380b2b809ea
Elkana Lumbantobing & Ken Duigraha Putra <pair+elkana-ltobing.kenduigraha@devbootcamp.com>
2016-10-12 16:51:29 +0700

    ORM symmetric

---
+++ migrations/20161012032848-create-student.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Students', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        type: Sequelize.TEXT
+      },
+      last_name: {
+        type: Sequelize.TEXT
+      },
+      gender: {
+        type: Sequelize.TEXT
+      },
+      birthday: {
+        type: Sequelize.TEXT
+      },
+      email: {
+        type: Sequelize.TEXT
+      },
+      phone: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Students');
+  }
+};
---
+++ models/student.js
@@ -0,0 +1,77 @@
+'use strict';
+var Sequelize = require('sequelize');
+require('sequelize-isunique-validator')(Sequelize);
+
+
+module.exports = function(sequelize, DataTypes) {
+  var Student = sequelize.define('Student', {
+    age: {
+        type: DataTypes.INTEGER,
+        validate: {
+          isAge: (value) => {
+            if(value < 5){
+              throw new Error('Student\'s Age is Have to Above 5 Years')
+            }
+          }
+        }
+    },
+    gender: DataTypes.TEXT,
+    birthday: DataTypes.TEXT,
+    email: {
+      type: DataTypes.TEXT,
+      validate: {
+        isEmail: true,
+        isUnique: sequelize.validateIsUnique('email')
+      }
+    },
+    phone: {
+        type: DataTypes.TEXT,
+        validate: {
+          len: {
+            args: [10,11],
+            msg: "Phone Length Should 10 Digits"
+          },
+          isInt: {
+            args: true,
+            msg: "Phone Length Should Integer"
+          }
+        }
+    }
+  },{
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      },
+      getName: function(){
+        Student.findAll({
+          attributes: ['first_name', 'last_name']
+        }).then((data, err) => {
+          if(err){
+            console.log(err);
+          }else{
+            for(var i = 0 ; i < data.length ; i++){
+              console.log(`${data[i].first_name} ${data[i].last_name}`);
+            }
+          }
+        })
+      },
+      getAge : function(){
+        Student.findAll({
+          attributes: ['age']
+        }).then((data, err) => {
+          if(err){
+            console.log(err);
+          }else{
+            for(var i = 0 ; i < data.length ; i++){
+              console.log(`${data[i].age}`);
+            }
+          }
+        })
+      }
+    }
+  });
+  // console.log(Student);
+  return Student;
+};
+
+// console.log(module.exports);
