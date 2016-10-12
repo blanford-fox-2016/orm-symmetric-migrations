1b008d21297b9af5e4d81fe370ec4ada4b18a4d0
Septian A Tama <tama@tamatamvan.web.id>
2016-10-12 17:12:38 +0700

    update method
    update method

--- models/student.js
+++ models/student.js
@@ -1,13 +1,13 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var Student = sequelize.define('Student', {
-    first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
+    name: DataTypes.STRING,
     birthday: DataTypes.DATEONLY,
     email: DataTypes.STRING,
     phone: DataTypes.STRING,
-    age: DataTypes.STRING,
-    gender: DataTypes.STRING
+    age: DataTypes.INTEGER,
+    gender: DataTypes.STRING,
+    address: DataTypes.STRING
   }, {
     classMethods: {
       associate: function(models) {
