f20678b0d2125b7d16b6e0ac9a9ec6286816469c
Juang Wiantoro & Mangku Widodo <pair+broerjuang.mangkuwi@devbootcamp.com>
2016-10-12 17:32:37 +0700

    final realese

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 'use strict'
 
+let models = require('./models');
+let Student = models.Student;
+
+Student.create({
+  name: "juang wiantoro",
+  age: 7,
+  email: "juangwiantoro@gmail.com",
+  phonenumber: 12345677890
+}).catch((err) => {
+  console.log("Please Check Your Data");
+})
---
+++ migrations/20161012100330-create-student.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Students', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstname: {
+        type: Sequelize.STRING
+      },
+      lastname: {
+        type: Sequelize.STRING
+      },
+      age: {
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      phonenumber: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Students');
+  }
+};
